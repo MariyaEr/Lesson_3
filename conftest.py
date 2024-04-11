7d55047502a2dd27bb1221a592dce89ea96a713d
mariya.ermolaeva <ermolaeva.mariya14@gmail.com>
2024-04-11 21:17:25 +0300

    Правки по замечаниям

--- conftest.py
+++ conftest.py
@@ -11,13 +11,13 @@ def window():
 
 @pytest.fixture()
 def browser_open():
-    browser.open('https://google.com')
+    browser.open('https://google.com/ncr')
     print('Открываем браузер')
 
 
 @pytest.fixture()
 def browser_size():
     options = webdriver.ChromeOptions()
-    options.add_argument("--window-size=300,300")
+    options.add_argument("--window-size=1000,1000")
     browser.config.driver_options = options
     print('Размер браузера')
--- test_main.py
+++ test_main.py
@@ -5,6 +5,6 @@ def test_search(browser_size, browser_open, window):
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 def test_incorrect(browser_open):
-    browser.element('[name="q"]').should(be.blank).type('tttss33333333ss33333').press_enter()
-    browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))
+    browser.element('[name="q"]').should(be.blank).type('gMvZjgрррSесв0Gcb1ErbpRtwZY4jI2о765оооpMa2xtsqWQ0y7hN5QM2A-gMEnACST66dLqгрпаKMtWYjFL2BrHfMa7dibCz2bCB3lLNfe-wgwQYKD1AkOnnIYSfa0EjvbFkK').press_enter()
+    browser.element('[id="result-stats"]').should(have.text('About 0 results'))
     print('Не найдено')
